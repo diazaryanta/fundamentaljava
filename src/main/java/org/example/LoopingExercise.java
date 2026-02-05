1ee7d4eb9ca239ef10be64f631fd394402d6535e
diaz <diazaryanta04@yahoo.com>
2026-02-06 04:20:02 +0700

    update 06-February-2026

--- src/main/java/org/example/LoopingExercise.java
+++ src/main/java/org/example/LoopingExercise.java
@@ -1,30 +1,29 @@
 package org.example;
 
-//Example 1
 public class LoopingExercise {
-    public static void main(String[] args) {
-        int countdown = 5;
 
-        while (countdown > 0) {
-            System.out.println(countdown);
-            countdown--;
+    public static void main(String[] args) {
+
+        //ForLoop();
+        WhileLoop();
+    }
+//SampleForLoop
+    static void ForLoop() {
+        System.out.println("=== MULAI FOR LOOP ===");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Perulangan ke-" + i);
         }
-        System.out.println("Happy New Year!!");
+        System.out.println("For Loop Selesai.");
+    }
+//SampleWhileLoop
+    static void WhileLoop() {
+        System.out.println("=== MULAI WHILE LOOP ===");
+        int bensin = 5;
+        while (bensin > 0) {
+            System.out.println("Menyalakan mesin... Sisa bensin: " + bensin + " liter");
+            bensin--;
+        }
+        System.out.println("Mobil mogok! Bensin habis.");
     }
 }
-
-//Example 2
-//public class LoopingExercise {
-//    public static void main(String[] args) {
-//        int dice = 1;
-//
-//        while (dice <= 6) {
-//            if (dice < 6) {
-//                System.out.println("Awas ada Diaz.");
-//            } else {
-//                System.out.println("TARAAAA");
-//            }
-//            dice = dice + 1;
-//        }
-//    }
-//}
